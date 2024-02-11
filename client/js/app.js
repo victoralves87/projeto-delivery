b7e5dbe219dfa85403370dc65317e3b497e3621c
victor alves <voctorralves@gmail.com>
2024-02-11 16:53:22 -0300

    fazendo autenticação login pt1

---
+++ client/js/app.js
@@ -0,0 +1,67 @@
+let app = {};
+
+
+app.event = {
+
+}
+
+
+app.method = {
+
+    //centraliza as chamadas de get
+    get: (url, callbackSuccess, callbackError, login = false) => {
+
+        try {
+            
+            if(app.method.validaToken(login)){
+                let xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+                xhr.setRequestHeader("Authorization", app.method.obterValorStorage('token'));
+
+                xhr.onreadystatechange = function (){
+                    if(this.readyState == 4){
+                        if(this.status == 200){
+                            return callbackSuccess(JSON.parse(xhr.responseText))
+                        }
+                        else{
+                            //se o retorno nao for autorizado, redireciona o usuário para o login
+                            if(xhr.status == 401){
+                                app.method.logout(); 
+                            }
+                            return callbackError(xhr.responseText);
+                        }
+                    }
+                }
+            }
+            
+        } catch (error) {
+            
+        }
+    },
+
+    //metodo para validar se o token existe(local)
+    validaToken: (login = false) => {
+        var tokenAtual = app.method.obterValorStorage('token');
+
+        if((tokenAtual == undefined || tokenAtual==null || tokenAtual=="" || tokenAtual=="null") && !login){
+            window.location.href = '/painel/login.html'; 
+            return false;
+        }
+        return true;
+    },
+
+    gravarValorStorage: (valor,local) => {
+        localStorage[local] = valor;
+    },
+
+    obterValorStorage: (local) => {
+        return localStorage[local];
+    },
+
+    //limpa todo localStorage e redireciona pro login
+    logout:() =>{
+        localStorage.clear();
+        window.location.href = '/painel/login.html'; 
+    }
+}
---
+++ client/painel/js/login.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    login.event.init();
+});
+
+let login ={
+
+}
+
+
+login.event ={
+    init: () => {
+        console.log("olá!")
+    }
+}
+
+
+login.method ={
+
+}
