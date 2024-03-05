c1f04fecca0740a9faad7a6f5f176e0e2cef5fa7
victor alves <voctorralves@gmail.com>
2024-03-05 10:41:49 -0300

    refazendo o backend

--- server/routes/login.js
+++
@@ -1,9 +0,0 @@
-const ct = require('../controllers/login');
-
-module.exports = (server) => {
-    
-    server.post('/login', async(req,res) => {
-        const result = await ct.controllers().login(req);
-        res.send(result);
-    })
-}
--- server/scripts/login.sql
+++
@@ -1,5 +0,0 @@
---INIT#login#
-
-SELECT idempresa, nome , email, senha, logotipo FROM empresa WHERE email = @email
-
---END#login#
